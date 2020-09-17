e39a6ab4d82bab55c130c6b288655a29a596f707
toggi <thorgilshjalmarsson@gmail.com>
2020-09-17 14:13:27 +0000

    Reitir

---
+++ tile-traveler.py
@@ -0,0 +1,11 @@
+#Global reitir
+square1_1 = "(N)orth"
+square1_2 = "(N)orth or (E)ast or (S)outh"
+square1_3 = "(E)ast or (S)outh"
+square2_1 = "(N)orth"
+square2_2 = "(S)outh or (W)est"
+square2_3 = "(W)est or (E)ast"
+square3_2 = "(N)orth or (S)outh"
+square3_3 = "(W)est or (S)outh"
+
+
