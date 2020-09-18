df093ef5cdd2e70be343b3b0439e25fae3291998
toggi <thorgilshjalmarsson@gmail.com>
2020-09-18 14:51:08 +0000

    umskrif

--- tile-traveler.py
+++ tile-traveler.py
@@ -1,4 +1,4 @@
-https://github.com/creep1g/TileTraveler
+#https://github.com/creep1g/TileTraveler
 #Global reitir
 square1_1 = "(N)orth."
 square1_2 = "(N)orth or (E)ast or (S)outh."
@@ -10,51 +10,53 @@ square3_1 = "(N)orth."
 square3_2 = "(N)orth or (S)outh."
 square3_3 = "(S)outh or (W)est."
 
-
 def walls(directions, row, column):
     '''Innri veggir - gefur True ef það má ekki fara í ákveðna átt, annars False'''
     if column == 1 and row == 1:
-        if directions.lower() == "e":
+        if directions == "e":
             return True
     elif column == 2 and row == 1:
-        if directions.lower() == "e":
+        if directions == "e":
             return True
-        elif directions.lower() == "w":
+        elif directions == "w":
             return True
     elif column == 2 and row == 2:
-        if directions.lower() == "e":
+        if directions == "e":
             return True
-        elif directions.lower() == "n":
+        elif directions == "n":
             return True
-    elif column == 2 and row == 3:       
-        if directions.lower() == "s":
+    elif column == 2 and row == 3:
+        if directions == "s":
             return True
     elif column == 3 and row == 2:
-        if directions.lower() == "w":
+        if directions == "w":
             return True
     else:
         return False
 
 def give_directions(row, column):
     '''Finnur staðsetningu leikmans og gefur viðeigandi skilaboð'''
-    if row == 1 and column == 1:
-        return square1_1
-    elif column == 1 and row == 2:
-        return square1_2
-    elif column == 1 and row == 3:
-        return square1_3
-    elif column == 2 and row == 1:
-        return square2_1
-    elif column == 2 and row == 2:
-        return square2_2
-    elif column == 2 and row == 3:
-        return square2_3
-    elif column == 3 and row == 1:
-        return square3_1
-    elif column == 3 and row == 2:
-        return square3_2
-    elif column == 3 and row == 3:
-        return square3_3
+    if column == 1:
+        if row == 1:
+            return square1_1
+        elif row == 2:
+            return square1_2
+        elif row == 3:
+            return square1_3
+    elif column == 2:
+        if row == 1:
+            return square2_1
+        elif row == 2:
+            return square2_2
+        elif row == 3:
+            return square2_3
+    elif column == 3:
+        if row == 1:
+            return square3_1
+        elif row == 2:
+            return square3_2
+        elif row == 3:
+            return square3_3
 
 def move_player(user_input, row, column):
     '''Athugar hvort leikamaður hefur valið mögulega átt skilar þeim hnitum ef svo er
